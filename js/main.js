205563bde745f1939d970f1bb1ae4d6baac9eaa5
shubhanshu <shubhanshukrssm10@gmail.com>
2024-08-20 19:00:54 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let toggle=()=>{
+    if (document.querySelector("#toggle").style.left=="100%") {
+        document.querySelector("#toggle").style.left="0"
+    } else {
+        document.querySelector("#toggle").style.left="100%"
+    }
+}
